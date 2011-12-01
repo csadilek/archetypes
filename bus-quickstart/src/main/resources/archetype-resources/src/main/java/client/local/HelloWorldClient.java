17f314f64eb7cfefd145714b0a55e255685cf8f7
Christian Sadilek <christian.sadilek@gmail.com>
2011-12-01 13:29:54 -0500

    fixed imports

--- bus-quickstart/src/main/resources/archetype-resources/src/main/java/client/local/HelloWorldClient.java
+++ bus-quickstart/src/main/resources/archetype-resources/src/main/java/client/local/HelloWorldClient.java
@@ -33,7 +33,7 @@ import org.jboss.errai.bus.client.api.Message;
 import org.jboss.errai.bus.client.api.MessageCallback;
 import org.jboss.errai.bus.client.api.base.MessageBuilder;
 import org.jboss.errai.bus.client.framework.MessageBus;
-import org.jboss.errai.bus.client.protocols.MessageParts;
+import org.jboss.errai.common.client.protocols.MessageParts;
 
 @EntryPoint
 public class HelloWorldClient extends VerticalPanel {
