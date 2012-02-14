38e5b3bb44cce5727c8c5775ffb56b01853a122a
Christian Sadilek <christian.sadilek@gmail.com>
2012-02-14 11:31:34 -0500

    Using a @Conversational response event

--- cdi-quickstart/src/main/resources/archetype-resources/src/main/java/client/shared/Response.java
+++ cdi-quickstart/src/main/resources/archetype-resources/src/main/java/client/shared/Response.java
@@ -1,12 +1,14 @@
 package ${package}.client.shared;
 
 import org.jboss.errai.common.client.api.annotations.Portable;
+import org.jboss.errai.enterprise.client.cdi.api.Conversational;
 
 /**
  * A marshallable bean that carries the response event from the server back to
  * the client.
  */
 @Portable
+@Conversational
 public class Response {
     private int id;
     private String message;
